ffec02e43ca70f3f2e5bc0e34c0b3687b7d3f052
Kai Tait <kaiftait@gmail.com>
2019-11-06 14:33:11 +1000

    Updates

---
+++ precache-manifest.821af98df74eded86720913d9f5ad2c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4006f05645b608def784d74b9e62b27f",
+    "url": "/titanite-react-redux/index.html"
+  },
+  {
+    "revision": "5ae5a3a6fd152d0319ef",
+    "url": "/titanite-react-redux/static/css/main.bd250669.chunk.css"
+  },
+  {
+    "revision": "aebf2540258ace595ab6",
+    "url": "/titanite-react-redux/static/js/2.4639d626.chunk.js"
+  },
+  {
+    "revision": "5ae5a3a6fd152d0319ef",
+    "url": "/titanite-react-redux/static/js/main.887852b0.chunk.js"
+  },
+  {
+    "revision": "674ccaf0a9ad60d577a7",
+    "url": "/titanite-react-redux/static/js/runtime-main.a13f4230.js"
+  },
+  {
+    "revision": "4be539dccbc55cde63b542e9252aadff",
+    "url": "/titanite-react-redux/static/media/bg-tick.4be539dc.svg"
+  },
+  {
+    "revision": "9735da72b9afe9ddca12a8d27e4f2650",
+    "url": "/titanite-react-redux/static/media/logo.9735da72.svg"
+  }
+]);
